2a6adb2774497b2cbaef414f094e93d233a51628
Valeriy Harlamov <valera545@gmail.com>
2021-08-25 11:14:29 +0300

    Some fixed yet

--- src/components/users.jsx
+++ src/components/users.jsx
@@ -73,11 +73,12 @@ const Rows = props => {
 
 const checkNums = num => {
   const str = String(num)
+  const length = str.length
 
   if (str[0] === '0') return 'Сегодня никто с тобой не тусует'
-  if (str[0] === '1' && str.length === 1) return `Сегодня с тобой тусует 1 человек`
-  if (str[str.length - 2] === '1' && str.length > 1) return `Сегодня с тобой тусуют ${num} человек`
-  if (['2', '3', '4'].includes(str[str.length - 1])) return `Сегодня с тобой тусуют ${num} человека`
+  if (str[0] === '1' && length === 1) return `Сегодня с тобой тусует 1 человек`
+  if (str[length - 2] === '1' && length > 1) return `Сегодня с тобой тусуют ${num} человек`
+  if (['2', '3', '4'].includes(str[length - 1])) return `Сегодня с тобой тусуют ${num} человека`
   return `Сегодня с тобой тусуют ${num} человек`
 
 }
