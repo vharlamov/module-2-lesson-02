412c30839a38054e45ae50a509481a3f1cfff510
Valeriy Harlamov <valera545@gmail.com>
2021-11-02 14:05:58 +0300

    time format refactored+

--- src/App/utils/formatDate.js
+++ src/App/utils/formatDate.js
@@ -17,6 +17,22 @@ export default function formatDate(date) {
     "декабря"
   ]
 
+  const formatMins = (data) => {
+    const dataStr = String(data)
+    const lastNum = dataStr[dataStr.length - 1]
+
+    switch (lastNum) {
+      case "1":
+        return dataStr + " минуту"
+      case "2":
+      case "3":
+      case "4":
+        return dataStr + " минуты"
+      default:
+        return dataStr + " минут"
+    }
+  }
+
   const formatNull = (data) => {
     const dataStr = String(data)
     return dataStr.length < 2 ? "0" + dataStr : dataStr
@@ -24,7 +40,7 @@ export default function formatDate(date) {
 
   switch (true) {
     case diffTime / 60 <= 10:
-      return ` - ${Math.floor(diffTime / 60)} минут назад`
+      return ` - ${formatMins(Math.floor(diffTime / 60))} назад`
     case diffTime / 3600 < 1 && diffTime / 3600 > 0.5:
       return ` - 30 минут назад`
     case diffTime / 86400 < 1:
