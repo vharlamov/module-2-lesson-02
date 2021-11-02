bd964119518a4fe5de5b2f36ed238daff30dd399
Valeriy Harlamov <valera545@gmail.com>
2021-11-02 13:36:30 +0300

    time format refactored+

--- src/App/utils/formatDate.js
+++ src/App/utils/formatDate.js
@@ -17,15 +17,22 @@ export default function (date) {
     "декабря"
   ]
 
+  const formatNull = (data) => {
+    const dataStr = String(data)
+    return dataStr.length < 2 ? "0" + dataStr : dataStr
+  }
+
   switch (true) {
     case diffTime / 60 < 1:
       return " - 0 минут назад"
     case diffTime / 60 <= 10:
       return ` - ${Math.floor(diffTime / 60)} минут назад`
-    case diffTime / 3600 < 1:
+    case diffTime / 3600 < 1 && diffTime / 3600 > 0.5:
       return ` - 30 минут назад`
     case diffTime / 86400 < 1:
-      return ` - ${created.getHours()}:${created.getMinutes()}`
+      return ` - ${formatNull(created.getHours())}:${formatNull(
+        created.getMinutes()
+      )}`
     case diffTime / 2592000 < 1:
       return ` - ${created.getDay()} ${months[created.getMonth()]}`
     case diffTime / 2592000 > 1:
