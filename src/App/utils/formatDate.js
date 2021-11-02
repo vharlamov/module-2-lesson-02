5d796e6a7679a16ccccd0bfb952c5a9ef092b3a2
Valeriy Harlamov <valera545@gmail.com>
2021-11-02 13:01:36 +0300

    time format refactored

--- src/App/utils/formatDate.js
+++ src/App/utils/formatDate.js
@@ -17,18 +17,20 @@ export default function (date) {
     "декабря"
   ]
 
-  if (diffTime / 60 < 1) return " - 0 минут назад"
-  if (diffTime / 60 <= 10) return ` - ${Math.floor(diffTime / 60)} минут назад`
-  if (diffTime / 3600 < 1) return ` - 30 минут назад`
-  if (diffTime / 86400 < 1) {
-    return ` - ${created.getHours()}:${created.getMinutes()}`
-  }
-  if (diffTime / 86400 > 1) {
-    return ` - ${created.getDay()} ${months[created.getMonth()]}`
-  }
-  if (diffTime / 2592000 > 1) {
-    return ` - ${created.getDay()} ${
-      months[created.getMonth()]
-    } ${created.getFullYear()}`
+  switch (true) {
+    case diffTime / 60 < 1:
+      return " - 0 минут назад"
+    case diffTime / 60 <= 10:
+      return ` - ${Math.floor(diffTime / 60)} минут назад`
+    case diffTime / 3600 < 1:
+      return ` - 30 минут назад`
+    case diffTime / 86400 < 1:
+      return ` - ${created.getHours()}:${created.getMinutes()}`
+    case diffTime / 86400 > 1:
+      return ` - ${created.getDay()} ${months[created.getMonth()]}`
+    case diffTime / 2592000 > 1:
+      return ` - ${created.getDay()} ${
+        months[created.getMonth()]
+      } ${created.getFullYear()}`
   }
 }
